f5c87c6f7224dcc196fb1ae30c3d5934f007dafe
Paul Marston <paul@marston.me>
2017-02-21 20:00:03 +0000

    wip

--- Internals/Get-AzureRmAccessToken.ps1
+++ Internals/Get-AzureRmAccessToken.ps1
@@ -2,8 +2,8 @@ function Get-AzureRmAccessToken {
     if ($null -eq [Microsoft.WindowsAzure.Commands.Common.AzureRmProfileProvider]::Instance.Profile.Context) {
         throw [System.Management.Automation.PSInvalidOperationException]::new("Run Login-AzureRmAccount to login.")
     }
-
-    [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext]::new(
+    
+  [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext]::new(
         (@([Microsoft.WindowsAzure.Commands.Common.AzureRmProfileProvider]::Instance.Profile.Context.Environment.GetEndpoint([Microsoft.Azure.Commands.Common.Authentication.Models.AzureEnvironment+Endpoint]::ActiveDirectory),
             [Microsoft.WindowsAzure.Commands.Common.AzureRmProfileProvider]::Instance.Profile.Context.Tenant.Id.Guid) -join ''),
             [Microsoft.IdentityModel.Clients.ActiveDirectory.TokenCache]::DefaultShared).AcquireToken(
